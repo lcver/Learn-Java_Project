cb6b71efd78fc1643a33b2e3542eae6477ee61a3
lcver <crackover1092@gmail.com>
2020-05-16 14:43:32 +0700

    Email Class

---
+++ src/emailapp/Email.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package emailapp;
+
+/**
+ *
+ * @author lucver
+ */
+public class Email {
+    
+    private String firstname;
+    private String lastname;
+    private String department;
+    private int mailBoxCapacity;
+    private String alternativeEmail;
+    
+    // Construct to receive the first name and last name
+    
+    
+    // Ask for department
+    
+    
+    // Set the mailbox capacity
+    
+    
+    // Set the alternative email
+    
+    
+    
+    // Change the password
+    
+    
+    
+}
