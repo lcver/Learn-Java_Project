425e01eac73ff01cf221ffbc7e66b9646ccbe743
lcver <crackover1092@gmail.com>
2020-05-17 03:02:16 +0700

    Set Department method

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -5,25 +5,52 @@
  */
 package emailapp;
 
+import java.util.Scanner;
+
 /**
  *
  * @author lucver
  */
 public class Email {
     
-    private String firstname;
-    private String lastname;
+    private String firstName;
+    private String lastName;
     private String department;
     private int mailBoxCapacity;
-    private String alternativeEmail;
     
     // Construct to receive the first name and last name
-    
+    public Email(String firstName, String lastName)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        
+        this.department = setDepartment();
+        
+        System.out.println("Email Created");
+        System.out.println("==========================");
+        System.out.println("Name : "+this.firstName+" "+this.lastName);
+        System.out.println("Department : "+this.department);
+        System.out.println("==========================");
+    }
     
     // Ask for department
+    public String setDepartment()
+    {
+        System.out.println("Enter the department\n1 for sales\n2 for Developer \n3 for none");
+        System.out.println("> ");
+        
+        Scanner input = new Scanner(System.in);
+        int choise = input.nextInt();
+        
+        if(choise == 1) {return "Sales";}
+        else if (choise == 2) {return "Developer";}
+        else if (choise == 3) {return "none";}
+        else {return "";}
+            
+    }
     
     
-    // Set the mailbox capacity
+    // Set the mailbox capacity example 500mb
     
     
     // Set the alternative email
